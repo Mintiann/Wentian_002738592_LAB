c59d7d6e2c4d43f6f4d00edb2c2d3cc1267fc63e
Mintiann <114694339+Mintiann@users.noreply.github.com>
2023-01-29 21:40:56 -0500

    Add files via upload

---
+++ Lab2/src/Model/Program.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.Arrays;
+/**
+ *
+ * @author hp
+ */
+public class Program {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6};
+        for(int i = 0; i < arr.length; i++){
+          if(i % 2 == 0){
+            arr[i] = arr[i] * 2;
+          }else{
+            arr[i] = arr[i] * 3;
+          }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
